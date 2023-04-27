553723abf9a2c3b3d9af95cc10a73813483720f2
Dylan-Walters-Student <112421363+Dylan-Walters-Student@users.noreply.github.com>
2023-04-27 12:02:34 -0400

    started state driven cameras and movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float tankSteer = 250f;
+    [SerializeField] float tankSpeed = 10f;
+    [SerializeField] float swerveSteer = 10f;
+    [SerializeField] float swerveSpeed = 10f;
+    void Start()
+    {
+    }
+    void Update()
+    {
+        // TankDrive();
+        SwerveDrive();
+    }
+
+    void TankDrive()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * tankSteer * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * tankSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
+
+    void SwerveDrive()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * swerveSteer * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * swerveSpeed * Time.deltaTime;
+        transform.Translate(horizontal, vertical, 0);
+    }
+}
