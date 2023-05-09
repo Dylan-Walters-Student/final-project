f3e91b2d88ab436a128ad3523d710c1a6e645732
Dylan_W <dywa55d@student.oaisd.org>
2023-05-08 20:28:28 -0400

    Simple name changes and tweaks

--- Assets/Scripts/SceneSwitch.cs
+++ Assets/Scripts/SceneSwitch.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class SceneSwitch : MonoBehaviour
+public class SceneManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
